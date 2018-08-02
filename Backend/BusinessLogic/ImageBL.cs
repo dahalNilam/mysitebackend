8597147aa805f0a5f9c8496fcd04c7f541f09ae6
nilam <dahalnilam90@gmail.com>
2018-08-02 13:53:33 -0700

    used user secret for connection string

---
+++ Backend/BusinessLogic/ImageBL.cs
@@ -0,0 +1,23 @@
+﻿namespace Backend.BusinessLogic
+{
+    using Backend.Modals;
+    using Microsoft.AspNetCore.Mvc;
+    using System.IO;
+    using System.Linq;
+
+    public static class ImageBL
+    {
+        private static readonly string BasePath = @"E:\Projects\MySite\Images\";
+
+        public static FileStreamResult GetImageFile(Image image)
+        {
+            var filePath = BasePath + image.FileName;
+
+            var ext = filePath.Split(".").LastOrDefault();
+
+            var mimeType = "image/" + ext;
+
+            return new FileStreamResult(new FileStream(filePath, FileMode.Open), mimeType);
+        }
+    }
+}
--- Backend/Controllers/ImageController.cs
+++ Backend/Controllers/ImageController.cs
@@ -1,15 +1,10 @@
 ﻿namespace Backend.Controllers
 {
     using Backend.Accessors;
+    using Backend.BusinessLogic;
     using Backend.Modals;
     using Microsoft.AspNetCore.Mvc;
-    using System;
     using System.Collections.Generic;
-    using System.IO;
-    using System.Net;
-    using System.Net.Http;
-    using System.Net.Http.Headers;
-    using System.Threading.Tasks;
 
     [Route("api/[controller]")]
     [ApiController]
@@ -30,20 +25,11 @@
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetBtId(int id)
+        public ActionResult GetBtId(int id)
         {
             var image = new ImageAccessor(_context).GetById(id);
 
-            var filePath = $@"E:\Projects\MySite\Images\" + image.FileName;
-
-            var memory = new MemoryStream();
-            using (var stream = new FileStream(filePath, FileMode.Open))
-            {
-                await stream.CopyToAsync(memory);
-            }
-            memory.Position = 0;
-
-            return File(memory, "image/jpg", Path.GetFileName(filePath));
+            return ImageBL.GetImageFile(image);
         }
 
         [HttpPut("{id}")]
